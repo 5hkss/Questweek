830d8416851896b8efb6182c8ffcc0d42ec7df3e
5hkss <g9fbt5bvnr@privaterelay.appleid.com>
2026-07-28 01:34:45 +0400

    Refresh offline cache for QuestWeek 2.0

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
-const CACHE_NAME = "questweek-v1";
+const CACHE_NAME = "questweek-v2";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
+  "./questweek-v2.css",
+  "./questweek-v2.js",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("./index.html")))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html"))));
 });
